b5e6a2e97da35acaa4f8a49e4162240eee156438
Mustafa Youssef <mustafa.youssef@creiden.com>
2022-12-23 09:17:06 +0200

    handle build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://api.themoviedb.org/3',
+  imageUrl: 'https://image.tmdb.org/t/p',
+  API_KEY:'5d900a04f25518f6ff167c2637b95f96',
+  youTubeEmbed: 'https://www.youtube.com/embed',
+  vimeoEmbed: 'https://vimeo.com/api/oembed.json'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   imageUrl: 'https://image.tmdb.org/t/p',
   API_KEY:'5d900a04f25518f6ff167c2637b95f96',
   youTubeEmbed: 'https://www.youtube.com/embed',
-  vimeoEmbed: 'https://vimeo.com/api/oembed.json  '
+  vimeoEmbed: 'https://vimeo.com/api/oembed.json'
 };
 
 /*
